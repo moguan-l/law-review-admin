22042b7cf3a03f571f7ef3a98634e7216f1d6d64
LQJ <1530212104@qq.com>
2018-04-07 23:34:46 +0800

    添加页面

--- bin/server.js
+++ bin/server.js
@@ -10,6 +10,6 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(staticPath, './index.html'))
 });
 
-app.listen(8080, function () {
+app.listen(8086, function () {
     console.info(process.env.NODE_ENV)
 });
